c26e1ceb9567ad838d8c84ec042e7ad90cbd1ec4
吴小白 <296015668@qq.com>
2025-01-18 12:40:03 +0800

    feat: 添加 opencloudos-23 rpm 包构建

--- build-docker.sh
+++ build-docker.sh
@@ -2,6 +2,38 @@
 #
 DOCKER_VERSION=v27.0.1
 
+BUILD_DEB=0
+BUILD_RPM=0
+
+while [[ $# > 0 ]]; do
+    lowerI="$(echo $1 | awk '{print tolower($0)}')"
+    case $lowerI in
+        -h|--help)
+            echo "Usage: $0 [OPTIONS]"
+            echo
+            echo "Global Options:"
+            echo -e "  -h, --help  \t Show this help message and exit"
+            echo -e "  --deb  \t Build Debian-trixie deb package"
+            echo -e "  --rpm  \t Build OpenCloudOS-23 rpm package"
+            exit 0
+            ;;
+        --deb)
+            BUILD_DEB=1
+            shift
+            ;;
+        --rpm)
+            BUILD_RPM=1
+            shift
+            ;;
+        *)
+            echo "install: Unknown option $1"
+            echo "eg: $0 --deb --rpm"
+            exit 1
+            ;;
+    esac
+    shift
+done
+
 ################################################################
 # REF: v27.0.1
 # VERSION: 27.0.1
@@ -15,6 +47,7 @@ TMPDIR=$(mktemp -d)
 
 git clone --depth=1 https://github.com/docker/docker-ce-packaging "${TMPDIR}"
 cp -R debian-trixie "${TMPDIR}/deb/"
+cp -f opencloudos-23/Dockerfile "${TMPDIR}/rpm/fedora-41/Dockerfile"
 
 pushd "${TMPDIR}" || exit 1
 
@@ -35,11 +68,22 @@ GO_IMAGE=golang:${GO_VERSION}-trixie
 sed -i 's@DEBIAN_VERSIONS ?= debian-bullseye@DEBIAN_VERSIONS ?= debian-trixie debian-bullseye@g' deb/Makefile
 sed -i 's@docker build @docker buildx build --load @g' deb/Makefile
 
-make ARCH=loong64 ARCHES=loong64 REF=${REF} VERSION=${VERSION} GO_VERSION=${GO_VERSION} GO_IMAGE=${GO_IMAGE} debian-trixie
+if [ "${BUILD_DEB}" = '1' ]; then
+    make ARCH=loong64 ARCHES=loong64 REF=${REF} VERSION=${VERSION} GO_VERSION=${GO_VERSION} GO_IMAGE=${GO_IMAGE} debian-trixie
+fi
+if [ "${BUILD_RPM}" = '1' ]; then
+    make ARCH=loong64 ARCHES=loong64 REF=${REF} VERSION=${VERSION} GO_VERSION=${GO_VERSION} GO_IMAGE=${GO_IMAGE} fedora-41
+fi
 
 popd || exit 1
 
 mkdir -p dist
-mv ${TMPDIR}/deb/debbuild/debian-trixie/* dist/
+
+if [ "${BUILD_DEB}" = '1' ]; then
+    mv ${TMPDIR}/deb/debbuild/debian-trixie/* dist/
+fi
+if [ "${BUILD_RPM}" = '1' ]; then
+    mv ${TMPDIR}/rpm/rpmbuild/fedora-41/* dist/
+fi
 
 rm -rf "${TMPDIR:?}"
